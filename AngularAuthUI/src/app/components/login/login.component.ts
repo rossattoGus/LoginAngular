860d0a912824034ff7fae999d63da37b089ef77d
Gustavo Rossatto <rossatto.gus@gmail.com>
2025-03-16 13:43:13 -0300

    Mudança de versão: Angular 14.2.13 e Node 16.20.2; reescrita da configuração: mapeamentos, links CDNs, pastas de organização; Construção da página de login

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
